56bbcb3bcf7f73cb027fc469ab304e6778ce89d8
Gunadeshwar A <deshwardeshwar5@gmail.com>
2023-05-07 11:14:34 +0530

    ''

---
+++ src/Components/Home.js
@@ -0,0 +1,71 @@
+import React from "react";
+import './Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faBasketballBall } from '@fortawesome/free-solid-svg-icons';
+
+// use the icon
+
+
+
+function Home(){
+    return(
+
+        <nav>
+            <div className="wrapper">
+            <h3 className="logo">guna</h3>
+            <ul className="d-flex ">
+                
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Projects</li>
+            </ul>
+            </div>
+            <div className="Titlecard">
+                <div className="box1">
+                    <div className="big_box1">
+                        <div className="heading">
+                        Hey,I'm Abo,a product designer with 7 years of experince
+                        </div>
+                        <div className="small_box1">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        </div>
+
+                    </div>
+                    <div className="big_box2">
+               
+
+                        <div className="font_img">
+                        <button>Contact Me</button>
+
+                        
+                        <div className="font_bg">
+                        <FontAwesomeIcon  icon={faInstagram} style={{ fontSize: '30px' }} />
+                        </div>
+                        <div className="font_bg">
+                        <FontAwesomeIcon icon={faTwitter} style={{ fontSize: '30px' }} />
+                        </div>
+                        <div className="font_bg">
+                        <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: '30px' }} />
+                        </div>
+                        <div className="font_bg">
+                        <FontAwesomeIcon icon={faBasketballBall} style={{ fontSize: '30px' }}/>
+                        </div>
+                        </div>
+                    
+                   
+                    
+                   
+                    </div>
+                </div>
+                <div className="box2">
+                    
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Home;
