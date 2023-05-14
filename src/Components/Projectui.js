ca4d9124f431f57f411f6b4b863ea8e687909936
Gunadeshwar A <deshwardeshwar5@gmail.com>
2023-05-14 20:02:24 +0530

    changes

--- src/Components/Projectui.js
+++ src/Components/Projectui.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Projectui.css'
-import netfliximg from '../Images/download.jpg'
 function Projectui(props) {
   return (
     <div>
         <div className="container">
           <a href={props.url}>
             <div className='project_image'>
-                <img src={props.image}/>
+                <img src={props.image} alt=''/>
             </div>
             <div className='title'>
               <h1>{props.name}</h1></div>
