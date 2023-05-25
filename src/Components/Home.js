9f90c7e65b042bc907e38e42d6151c4501fc53bb
Gunadeshwar A <deshwardeshwar5@gmail.com>
2023-05-25 21:55:03 +0530

    chnages

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -28,9 +28,8 @@ function Home(){
             <div className="Titlecard">
                 <div className="box1">
                     <div className="big_box1">
-                        <div className="heading">
-                        Hey,I'm Deshwar,a Web Developer with Hands on experince
-                        </div>
+                        <div className="heading typing-animation">
+                        Hey,I'm Deshwar,a Web Developer </div>
                         <div className="small_box1">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
                         </div>
